58552efc522b7b5f48d33b2ab80efa5104994c18
Marta1717 <martaolejn@gmail.com>
2024-07-21 18:12:53 +0200

    forms

---
+++ src/main/java/pl/coderslab/HomeController.java
@@ -0,0 +1,20 @@
+package pl.coderslab;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+@Slf4j
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showForm() {
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String addForm() {  //@ModelAttribute Model model, BindingResult bindingResult) {
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%--<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>--%>
+<%--<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>--%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+</head>
+<body>
+<h1>Observation Journal</h1>
+</body>
+</html>
