67050c186abd50cf82f56ca3c8c3f80acf616763
Marta1717 <martaolejn@gmail.com>
2024-10-30 15:01:16 +0100

    Refactoring discussion controller - discussion/add

--- src/main/java/pl/coderslab/Discussion/DiscussionController.java
+++ src/main/java/pl/coderslab/Discussion/DiscussionController.java
@@ -49,6 +49,7 @@ public class DiscussionController {
         return "addDiscussion";
     }
 
+
     @PostMapping(value = "/observation/discussion/add")
     public String processAddDiscussion(@ModelAttribute Discussion discussion, HttpSession session, @RequestParam Long id) {
         User loggedInUser = (User) session.getAttribute("loggedInUser");
